f61bcb121cb4b118d808d02b48c36830817ad37f
ssilva22 <sebastianwebp@gmail.com>
2021-08-17 22:36:52 +0000

    ready for deployment

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,10 +1,9 @@
-class ChatroomController < 
+class ChatroomController < ApplicationController
   before_action :require_user
-  def show
-  end
 
   def index
     @message = Message.new
-    @messages = Message.all
+    @messages = Message.custom_display
   end
+
 end
